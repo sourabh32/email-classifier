49003d2fe7199a15095ca5ba8474084b15d04d72
sourabh@32 <sourabhsharma181003@gmail.com>
2024-06-09 16:08:12 +0530

    fix

--- app/mails/page.tsx
+++ app/mails/page.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 import { Button } from "@/components/ui/button";
 import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
